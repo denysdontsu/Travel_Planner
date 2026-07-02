38cef86287f8ba5d6214e3047b432506382f752c
Denys Dontsu <dendontsi01@gmail.com>
2026-07-02 21:26:02 +0300

    refactor(database): remove unused Base declaration

--- app/core/database.py
+++ app/core/database.py
@@ -8,16 +8,13 @@ from sqlalchemy.orm import DeclarativeBase
 # App
 from app.core.config import settings
 
-engine = create_async_engine(settings.database_url,
+engine = create_async_engine(settings.DATABASE_URL,
                              echo=settings.DEBUG,
                              pool_pre_ping=True)
 
 async_session_maker = async_sessionmaker(engine,
                                          expire_on_commit=False,
                                          class_=AsyncSession)
-
-class Base(DeclarativeBase):
-    pass
 
 
 async def get_db() -> AsyncGenerator[AsyncSession, None]:
